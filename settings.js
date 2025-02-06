d095ff4a56c68149241eb4c9e6d4e3718b71c970
SYKO CODER <121161813+SYKO-CODER@users.noreply.github.com>
2025-02-06 22:26:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'CWQm2IDS#1CWJGmP8E9RjLBj0h6-k3VM1TeXb23lOnVRTZvllk7Appp' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
